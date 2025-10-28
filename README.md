8aad2c544b4d9ecfc6d9da94e932389f59333b18
Jordi Deans Mursalim <jordideans@gmail.com>
2025-10-27 17:41:32 -0700

    Add files via upload
    reformat git page

--- README.md
+++ README.md
@@ -1,34 +1,30 @@
 # la_casa_lobot
 discord bot for my friends' and I's server
 
-Nothing fancy, integrates some movie watchlist commands into our discord server
-The bot is being hosted on a digital ocean droplet
+Integrates some movie watchlist commands into our discord server
 
 the commands so far are:
 
-$watchlist username
+**/watchlist username**
 
-takes a letterboxd username and webscrapes for the watchlist of the user
+*takes a letterboxd username and webscrapes for the watchlist of the user*
 
-$watchlist_pool username1,username2,...
+**/watchlist_pool username1,username2,...**
 
-takes a list of letterboxd usernames separated by a comma and webscrapes for the watchlist of the users
+*takes a list of letterboxd usernames separated by a comma and webscrapes for the watchlist of the users*
 
-$random_watchlist username
+**$random_watchlist username**
 
-webscrapes for the watchlist, then returns a random movie
+*webscrapes for the watchlist, then returns a random movie*
 
-$random_watchlist_pool username1,username2,...
+**$random_watchlist_pool username1,username2,...**
 
-webscrapes multiple users letterboxd watchlists then prints a random movie, ensuring no duplicates are present in the original watchlist
+*webscrapes multiple users letterboxd watchlists then prints a random movie, ensuring no duplicates are present in the original watchlist*
 
-$coinflip
+**$coinflip**
 
-simple coinflip with a little animation to spice it up
+*simple coinflip with a little animation to spice it up*
 
-Recommendation Commands:
+**/recommend movie_title number_of_movies**
 
-Currently working on some commands that integrate a recommendation algorithm to suggest a movie based on content filtering
-It will use the TMDB API to make a csv file containing a lot of movies.
-
-The miner.py file is what I used to make the csv file
+*recommend a list of movies*
---
+++ src/data_collection/scraper.py
@@ -0,0 +1,55 @@
+# Jordi Mursalim 2025
+# Webscraper takes data from letterboxd
+# Used to get the list of movies on a user's watchlist, the data scraping can be changed to include other details of a movie
+
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
+
+# load the .env file that holds the webscraping permissions
+load_dotenv()
+user_agent = os.getenv('USER_AGENT')
+accept_language = os.getenv('ACCEPT_LANGUAGE')
+
+# function to get the movie data from a letterboxd watchlist with the parameters of the username and watchlist page
+def get_movie_details(username: str, page_number) -> dict:
+    
+    # credentials for beautiful soup to send requests to letterboxd
+    headers = {
+        'User-Agent': user_agent,
+        'Accept-Language': accept_language
+    }
+
+    # url of letterboxd watchlists
+    watchlist_url = f'https://letterboxd.com/{username}/watchlist/page/{page_number}/'
+
+    # initialize empty watchlist
+    movie_details = []
+
+    # initialize beautiful soup of letterboxd watchlist
+    page = requests.get(watchlist_url, headers=headers)
+    soup = BeautifulSoup(page.content, features='lxml')
+
+    try:
+        # find poster-container class in html
+        posters = soup.find("ul", {"class": "grid -p125 -scaled128"})
+
+        # iterate through each poster
+        for item in posters.find_all("li", {"class": "griditem"}):
+            
+            # this div has the movie details
+            comp = item.find("div", {"class": "react-component"})
+
+            # get the movie title from the alt text of the image
+            if comp:
+                title = comp.get("data-item-full-display-name")
+                movie_details.append(title)
+                
+        return movie_details
+
+    except Exception as e:
+        print('Could not fetch watchlist details')
+        print(f'Failed with exception: {e}')
+
+        return ValueError
